a934764cc33afef3c26ea9105f55e77a0bbd75c6
Mr.Kittipan Sukhantharot <hlKittipan@users.noreply.github.com>
2021-05-21 20:35:53 +0700

    add language

--- configs/index.js
+++ configs/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 4000,
-  URL: process.env.BASE_URL || 'http://localhost:4000',
+  URL: process.env.BASE_URL || 'http://localhost:4000/',
   MONGODB_URI: process.env.MONGODB_URI || '',
   MONGODB_LOCAL: process.env.MONGODB_LOCAL || process.env.MONGODB_URI,
   JWT_SERCRET: process.env.JWT_SERCRET || 'secret1',
